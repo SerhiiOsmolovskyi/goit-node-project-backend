89fd75d4d7fdeb7cddac29fecfe41c457b377334
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2024-05-13 13:10:09 -0600

    init project

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import moviesRouter from "./routes/moviesRouter.js";
+import contactsRouter from "./routes/contactsRouter.js";
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/movies", moviesRouter);
+app.use("/api/contacts", contactsRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});
+
+const DB_HOST =
+  "mongodb+srv://Osmolovskyi:Aeb_137894@cluster0.on0w9zy.mongodb.net/my-movies?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
